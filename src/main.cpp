7ca9f3241d5e0ddf9f2fcb94e23c6e915440665e
eliasagustin <30348974+eliasagustin@users.noreply.github.com>
2024-07-01 02:04:17 -0300

    done

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,27 @@
 #include <Arduino.h>
+#include <FastLED.h>
+#include <driver/gpio.h>
 
+#define DATA_PIN GPIO_NUM_18
+#define NUM_LEDS 1
 // put function declarations here:
-int myFunction(int, int);
-
+int i=0;
+CRGB leds[1];
 void setup() {
-  Serial.begin(921600);
+  Serial.begin(115200);
   // put your setup code here, to run once:
-
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  
   Serial.println("Fin Setup");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 delay(1000);
-Serial.println(myFunction(2, 3));
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+if (i==0) {leds[0] = CRGB(255, 0, 0);}
+if (i==1) {leds[0] = CRGB(0, 255, 0);}
+if (i==2) {leds[0] = CRGB(0, 0, 255);}
+FastLED.show();
+Serial.println(i);
+if (i>=2) {i=0;} else {i++;}
 }
